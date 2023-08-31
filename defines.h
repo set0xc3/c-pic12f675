6e1e1e1dc7e8d09b1e8d51330e75e3b527325b77
set0xc3 <set0xc3@gmail.com>
2023-08-31 12:59:01 +0300

    init

---
+++ defines.h
@@ -0,0 +1,33 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef int8_t  i8;
+typedef int16_t i16;
+typedef int32_t i32;
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+
+typedef float  f32;
+
+typedef i8  b8;
+typedef i16 b16;
+typedef i32 b32;
+
+#define Bytes(value)     (value)
+#define Kilobytes(value) (value << 10)
+#define Megabytes(value) (value << 20)
+#define Gigabytes(value) ((u64)(value) << 30)
+#define Terabytes(value) ((u64)(value) << 40)
+
+#define ArrayCount(a) (sizeof((a)) / sizeof(*(a)))
+
+#define Clamp(value, min, max)                                                \
+    ((value <= min) ? min : (value >= max) ? max : value)
+#define ClampTop(value, max) ((value >= max) ? max : value)
+
+#endif // DEFINES_H
---
+++ main.c
@@ -0,0 +1,74 @@
+/*
+ * File:   main.c
+ * Author: duck
+ *
+ * Created on May 24, 2023, 2:46 AM
+ */
+
+// CONFIG
+#pragma config FOSC     = INTRCIO
+#pragma config WDTE     = OFF
+#pragma config PWRTE    = OFF
+#pragma config MCLRE    = OFF
+#pragma config BOREN    = OFF
+#pragma config CP       = OFF
+#pragma config CPD      = OFF
+
+#define _XTAL_FREQ 4000000
+
+#include <xc.h>
+
+#include "defines.h"
+
+#define CLOCK_SPEED 4000000
+ 
+const u8 time_slot_divider  = (1000000 / (CLOCK_SPEED / 64));
+const u8 led_pin            = 4;
+
+void interrupt isr()
+{
+    RP0 = 0;
+    T0IE = 0;
+    T0IF = 0;
+    TMR0 = 0xFF;
+    if (INTF)
+    {
+        GPIO4 = ~GPIO4;
+        GPIO2 = 0;
+        INTF = 0;
+    }
+}
+
+void
+main()
+{
+    RP0 = 0;
+    GPIO = 0;
+    CMCON = 0x07; // Disable comparator
+    
+    RP0 = 1;
+    ANSEL = 0; // Set ports as digital I/O, not analog input
+    TRISIO = 0; // GP3 input, rest all output
+
+    RP0 = 0;
+    TMR0 = 0xFF;
+    INTCON = 0;
+    GIE = 1;
+    T0IE = 1;
+    PEIE = 1;
+    INTE = 1;
+    
+    RP0 = 1;
+    OPTION_REG = 0x7;
+    INTEDG = 1;
+    
+    while (1)
+    {
+        RP0 = 0;
+        T0IE = 1;
+        
+        __delay_ms(500);
+        GPIO4 = ~GPIO4;
+        __delay_ms(500);
+    }
+}
